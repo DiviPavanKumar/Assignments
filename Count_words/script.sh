a35ead6a6e58eff9f80705eba24039808e89e528
PavanDivi-DevOps <divipavankumar92@gmail.com>
2025-05-23 18:36:23 +0530

    Adding Files

--- Count_words/script.sh
+++ Count_words/script.sh
@@ -3,9 +3,9 @@
 # === CONFIGURATION ===
 GITHUB_USER="DiviPavanKumar"
 REPO="Assignments"
-BRANCH="c1171a4c465cf730d11f0e6023818d5ec9fcb83b"
-DIR_PATH="Assignments/Count_words"
-EXCLUDE_FILE="script.sh"  # file to exclude from listing
+BRANCH="master"
+DIR_PATH="Count_words"
+EXCLUDE_FILE="script.sh"
 
 # === COLORS & STYLES ===
 RED="\033[31m"
@@ -16,10 +16,8 @@ CYAN="\033[36m"
 BOLD="\033[1m"
 RESET="\033[0m"
 
-# === API URL ===
 API_URL="https://api.github.com/repos/$GITHUB_USER/$REPO/contents/$DIR_PATH?ref=$BRANCH"
 
-# === Get file list from GitHub API and exclude EXCLUDE_FILE ===
 echo -e "${YELLOW}Fetching file list from GitHub...${RESET}"
 mapfile -t FILES < <(curl -s "$API_URL" | grep '"name":' | cut -d '"' -f 4 | grep -v "^$EXCLUDE_FILE$")
 
@@ -28,7 +26,6 @@ if [ ${#FILES[@]} -eq 0 ]; then
     exit 1
 fi
 
-# === File selection loop ===
 while true; do
     echo -e "\n${GREEN}Available Files:${RESET}"
     for i in "${!FILES[@]}"; do
